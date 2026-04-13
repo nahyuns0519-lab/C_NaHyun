4db871254235076e7ced81cf66c38f1052823802
nahyuns0 <nahyuns0519@gmail.com>
2026-04-13 16:39:58 +0900

    Lab 3-1 인구수 출력 실습

---
+++ ch3/ch3_pj7_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ch = '#';
+    printf("%d\n", ch);
+    printf("%c\n", ch);
+
+    int pop1 = 51800000;
+    long long pop2 = 7716600000;
+    printf("%d %lld\n", pop1, pop2);
+
+    return 0;
+}
